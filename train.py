7dd5690fd3c36f258aa3a2056cc3bb5fc4f91e29
Wonseok Choi <zzang@WS_Laptop.localdomain>
2022-08-01 19:25:35 +0900

    first

---
+++ train.py
@@ -0,0 +1,204 @@
+# 라이브러리 추가
+from torchvision import transforms, datasets
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch
+from curses import cbreak
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+lr = 1e-3
+batch_size = 4
+num_epoch = 20
+
+data_dir = './datasets'
+ckpt_dir = './checkpoints'
+log_dir = './log'
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# 네트워크 구축
+
+
+class Unet(nn.Module):
+    def __init__(self):
+        super(Unet, self).__init__()
+
+        def CBR2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True):
+            layers = []
+            layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                                 kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)]
+            layers += [nn.BatchNorm2d(num_features=out_channels)]
+            layers += [nn.ReLU()]
+
+            cbr = nn.Sequential(*layers)
+
+            return cbr
+
+        # encoder part layer declare
+        self.enc1_1 = CBR2d(in_channels=1, out_channels=64)
+        self.enc1_2 = CBR2d(in_channels=64, out_channels=64)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc2_1 = CBR2d(in_channels=64, out_channels=128)
+        self.enc2_2 = CBR2d(in_channels=128, out_channels=128)
+
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc3_1 = CBR2d(in_channels=128, out_channels=256)
+        self.enc3_2 = CBR2d(in_channels=256, out_channels=256)
+
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc4_1 = CBR2d(in_channels=256, out_channels=512)
+        self.enc4_2 = CBR2d(in_channels=512, out_channels=512)
+
+        self.pool4 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc5_1 = CBR2d(in_channels=512, out_channels=1024)
+
+        # decoder part layer declare
+
+        self.dec5_1 = CBR2d(in_channels=1024, out_channels=512)
+
+        self.unpool4 = nn.ConvTranspose2d(
+            in_channels=512, out_channels=512, kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec4_2 = CBR2d(in_channels=2 * 512, out_channels=512)
+        self.dec4_1 = CBR2d(in_channels=512, out_channels=256)
+
+        self.unpool3 = nn.ConvTranspose2d(
+            in_channels=256, out_channels=256, kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec3_2 = CBR2d(in_channels=2 * 256, out_channels=256)
+        self.dec3_1 = CBR2d(in_channels=256, out_channels=128)
+
+        self.unpool2 = nn.ConvTranspose2d(
+            in_channels=128, out_channels=128, kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec2_2 = CBR2d(in_channels=2 * 128, out_channels=128)
+        self.dec2_1 = CBR2d(in_channels=128, out_channels=64)
+
+        self.unpool1 = nn.ConvTranspose2d(
+            in_channels=64, out_channels=64, kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec1_2 = CBR2d(in_channels=2 * 64, out_channels=64)
+        self.dec1_1 = CBR2d(in_channels=64, out_channels=64)
+
+        self.fc = nn.Conv2d(in_channels=64, out_channels=2,
+                            kernel_size=1, stride=1, padding=0, bias=True)
+
+    def forward(self, x):
+        enc1_1 = self.enc1_1(x)
+        enc1_2 = self.enc1_2(enc1_1)
+
+        pool1 = self.pool1(enc1_2)
+
+        enc2_1 = self.enc2_1(pool1)
+        enc2_2 = self.enc2_2(enc2_1)
+
+        pool2 = self.pool2(enc2_2)
+
+        enc3_1 = self.enc3_1(pool2)
+        enc3_2 = self.enc3_2(enc3_1)
+
+        pool3 = self.pool3(enc3_2)
+
+        enc4_1 = self.enc4_1(pool3)
+        enc4_2 = self.enc4_2(enc4_1)
+
+        pool4 = self.pool4(enc4_2)
+
+        enc5_1 = self.enc5_1(pool4)
+
+        dec5_1 = self.dec5_1(enc5_1)
+
+        unpool4 = self.unpool4(dec5_1)
+
+        cat4 = torch.cat((unpool4, enc4_2), dim=1)
+
+        dec4_2 = self.dec4_2(cat4)
+        dec4_1 = self.dec4_1(dec4_2)
+
+        unpool3 = self.unpool3(dec4_1)
+        cat3 = torch.cat((unpool3, enc3_2), dim=1)
+
+        dec3_2 = self.dec3_2(cat3)
+        dec3_1 = self.dec3_1(dec3_2)
+
+        unpool2 = self.unpool2(dec3_1)
+        cat2 = torch.cat((unpool2, enc2_2), dim=1)
+
+        dec2_2 = self.dec2_2(cat2)
+        dec2_1 = self.dec2_1(dec2_2)
+
+        unpool1 = self.unpool1(dec2_1)
+        cat1 = torch.cat((unpool1, enc1_2), dim=1)
+
+        dec1_2 = self.dec1_2(cat1)
+        dec1_1 = self.dec1_1(dec1_2)
+
+        x = self.fc(dec1_1)
+
+        return x
+
+
+# 데이터 로더 구현
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir
+        self.transform = transform
+
+        lst_data = os.listdir(self.data_dir)
+
+        lst_label = [f for f in lst_data if f.startswith('label')]
+        lst_input = [f for f in lst_data if f.startswith('input')]
+
+        lst_label.sort()
+        lst_input.sort()
+
+        self.lst_label = lst_label
+        self.lst_input = lst_input
+
+    def __len__(self):
+        return len(self.lst_label)
+
+    def __getitem__(self, index):
+        label = np.load(os.path.join(self.data_dir, self.lst_label[index]))
+        input = np.load(os.path.join(self.data_dir, self.lst_input[index]))
+
+        label = label/255.0
+        input = input/255.0
+
+        if label.ndim == 2:
+            label = label[:, :, np.newaxis]
+        if input.ndim == 2:
+            input = input[:, :, np.newaxis]
+
+        data = {'input': input, 'label': label}
+
+        if self.transform:
+            data = self.transform(data)
+
+        return data
+
+
+##
+dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'))
+
+##
+data = dataset_train.__getitem__(0)
+
+input = data['input']
+label = data['label']
+
+##
+plt.subplot(122)
+plt.imshow(input.squeeze())
+
+plt.subplot(121)
+plt.imshow(label.squeeze())
+
+plt.show()
